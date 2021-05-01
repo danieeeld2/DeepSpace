ff679df8ea9cc15015cbea98ac673201eb7342c9
danieeeld2 <danieeeld2@gmail.com>
2021-05-01 19:47:12 +0200

    Comprobado y funcional

--- Práctica2/lib/SpaceStation.rb
+++ Práctica2/lib/SpaceStation.rb
@@ -51,7 +51,7 @@ module Deepspace
       @hangar = nil
 
       # Añadimos los supplies
-      reciveSupplies(s)
+      receiveSupplies(s)
     end
 
     # Getters
@@ -183,7 +183,7 @@ module Deepspace
 
     # Decremente las unidades de combustible como consecuencia del desplazamiento
     def move
-      @fuelUnits -= @fuelUnits * speed
+      @fuelUnits -= @fuelUnits * getSpeed
       if @fuelUnits < 0
         @fuelUnits = 0
       end
@@ -218,3 +218,56 @@ module Deepspace
     private :assignFuelValue, :cleanPendingDamage
   end
 end
+
+# Código de prueba
+# supplies = Deepspace::SuppliesPackage.new(1,2,3)
+# prueba = Deepspace::SpaceStation.new("Prueba",supplies)
+# puts prueba.name
+# puts prueba.ammoPower
+# puts prueba.fuelUnits
+# puts prueba.nMedals
+# puts prueba.shieldPower
+# puts prueba.pendingDamage
+# puts prueba.weapons
+# puts prueba.shieldBoosters
+# puts prueba.hangar
+# puts prueba.to_s
+# hangar = Deepspace::Hangar.new(4)
+# weapon = Deepspace::Weapon.new('arma', Deepspace::WeaponType::PLASMA, 8)
+# shield = Deepspace::ShieldBooster.new('escudo', 1, 1)
+# puts prueba.receiveWeapon(weapon)
+# puts prueba.receiveShieldBooster(shield)
+# prueba.receiveHangar(hangar)
+# puts prueba.receiveWeapon(weapon)
+# puts prueba.receiveShieldBooster(shield)
+# puts prueba.hangar
+# prueba.mountWeapon(0)
+# prueba.mountShieldBooster(0)
+# puts prueba.hangar
+# puts prueba.weapons
+# puts prueba.shieldBoosters
+# prueba.discardHangar
+# puts prueba.hangar
+# prueba.receiveHangar(hangar)
+# puts prueba.receiveWeapon(weapon)
+# puts prueba.receiveShieldBooster(shield)
+# puts prueba.hangar
+# prueba.discardWeaponInHangar(0)
+# prueba.discardShieldBoosterInHangar(0)
+# puts prueba.hangar
+# puts prueba.getSpeed
+# puts prueba.fuelUnits
+# prueba.move
+# puts prueba.fuelUnits
+# weapon2 = Deepspace::Weapon.new('arma', Deepspace::WeaponType::PLASMA, 0)
+# puts prueba.receiveWeapon(weapon2)
+# prueba.mountWeapon(0)
+# puts prueba.weapons 
+# puts "---"
+# prueba.cleanUpMountedItems
+# puts prueba.weapons
+# puts prueba.validState
+# damage = Deepspace::Damage.newNumericWeapons(2,2)
+# prueba.setPendingDamage(damage)
+# puts prueba.pendingDamage
+# puts prueba.validState
