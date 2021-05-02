334ee01dad74a3894984a770183ab527b13e3c07
danieeeld2 <danieeeld2@gmail.com>
2021-05-02 12:48:42 +0200

    Pendiente de testear

--- Práctica3/lib/GameUniverse.rb
+++ Práctica3/lib/GameUniverse.rb
@@ -92,13 +92,137 @@ module Deepspace
 
     # Comprueba si la estación espacial jugando en ese turno cumple la condición de victoria
     # @return [Boolean] true en caso afirmativo, false en caso contrario
-    def hasAWinner
+    def haveAWinner
       if !@currentStation.nil?
         if @currentStation.nMedals >= @@WIN
           return true
         else
           return false
         end
+      end
+    end
+
+    ######## Métodos Prñactica 3 ############
+
+    # Descarta un arma de la estación espacial
+    # Solo lo hace si el estado del juego es INIT o AFTERCOMBAT
+    # @param i [Integer] índice del arma a descartar
+    def discardWeapon(i)
+      if state == GameState::INIT or GameState::AFTERCOMBAT
+        @currentStation.discardWeapon(i)
+      end
+    end
+
+    # Descarta un escudo de la estación espacial
+    # Solo lo hace si el estado del juego es INIT o AFTERCOMBAT
+    # @param i [Integer] índice del escudo a descartar
+    def discardShieldBooster(i)
+      if state == GameState::INIT or GameState::AFTERCOMBAT
+        @currentStation.discardShieldBooster(i)
+      end
+    end
+
+    # Inicia una partida
+    # @param names [Array<String>] nombres de los jugadores
+    # A cada jugador se le crea una estación espacial con los objetos necesarios
+    # Tambíen establece el primer enemigo y quien empieza la partida
+    def init(names)
+      if state == GameState::CANNOTPLAY
+        dealer = CardDealer.instance
+        names.each do |name|
+          supplies = dealer.nextSuppliesPackage
+          station = SpaceStation.new(name, supplies)
+          @spaceStations << station
+
+          nh = @dice.initWithNHangars
+          nw = @dice.initWithNWeapons
+          ns = @dice.initWithNShields
+
+          lo = Loot.new(0, nw, ns, nh, 0)
+          station.setLoot(lo)
+        end
+
+        @currentStationIndex = @dice.whoStarts(names.length)
+        @currentStation = @spaceStations[@currentStationIndex]
+        @currentEnemy = dealer.nextEnemy
+
+        @gameState.next(@turns, @spaceStations.length)
+      end
+    end
+
+    # Realiza el cambio de turno
+    # Debe comprobar que el jugador actual no tiene daño pendiente
+    # @return [Boolean] true si se cambia el turno, false en caso contrario
+    def nextTurn
+      if state == GameState::AFTERCOMBAT
+        if @currentStation.validState
+          @currentStationIndex = (@currentStationIndex + 1) % @spaceStations.length
+          @turns += 1
+
+          @currentStation = @spaceStations[@currentStationIndex]
+          @currentStation.cleanUpMountedItems
+
+          dealer = CardDealer.instance
+          @currentEnemy = dealer.nextEnemy
+
+          @gameState.next(@turns, @spaceStations.length)
+          return true
+        end
+        return false
+      end
+      return false
+    end
+
+    # Se realiza un combate entre la estación espacial y el enemigo
+    # @param station [SpaceSttion] estación espacial
+    # @param enemy [EnemyStarShip] enemgo
+    # @return [CombatResult] resultado del combate
+    def combatGo(station, enemy)
+      ch = @dice.firstShot
+
+      if ch == GameCharacter::ENEMYSTARSHIP
+        fire = enemy.fire
+        result station.receiveShot(fire)
+        if result == ShotResult::RESIST
+          fire = @currentStation.fire
+          result = enemy.receiveShot(fire)
+          enemyWins = (result == ShotResult::RESIST)
+        else
+          enemyWins = true
+        end
+      else
+        fire = station.fire
+        result = enemy.receiveShot(fire)
+        enemyWins = (result == ShotResult::RESIST)
+      end
+
+      if enemyWins
+        s = station.speed
+        moves = @dice.spaceStationMoves(speed)
+        if !moves
+          damage = enemy.damage
+          station.setPendingDamage(damage)
+          combatResult = CombatResult::ENEMYWINS
+        else
+          station.move
+          combatResult = CombatResult::STATIONESCAPES
+        end
+      else
+        aLoot = enemy.loot
+        station.setLoot(aLoot)
+        combatResult = CombatResult::STATIONWINS
+      end
+
+      @gameState.next(@turns, @spaceStations.length)
+      return combatResult
+    end
+
+    # Realiza un combate, si el juego se encuentra en un estado permitidp
+    def combat
+      if state == GameState::BEFORECOMBAT or GameState::INIT
+        return combatGo(@currentStation, @currentEnemy)
+      else
+        return CombatResult::NOCOMBAT
       end
     end
 
