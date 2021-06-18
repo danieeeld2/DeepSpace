d231d1c773efd3b2e3ff79469a214edd365c249d
danieeeld2 <danieeeld2@gmail.com>
2021-06-18 17:46:24 +0200

    Inspirado en DanielSP

---
+++ Práctica5/java/src/View/GUI/CombatElementView.java
@@ -0,0 +1,14 @@
+/**
+ * @file CombatElementView.java
+ * @author Daniel Pérez Ruiz
+ * @version P-05
+ * type : Java-Interface
+ */
+package View.GUI;
+
+/**
+ * @brief Interfaz utilizada por WeaponView y por ShieldView
+ */
+interface CombatElementView {
+    public boolean isSelected();
+}
